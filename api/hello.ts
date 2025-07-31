c4ff8e4bdb6185ca29e0e2fe568035093e8d4ad9
Timotej Rojko <33236065+timotejroiko@users.noreply.github.com>
2025-07-31 13:30:00 -0300

    ?

--- api/hello.ts
+++ api/hello.ts
@@ -1,9 +1,10 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node'
-import { version } from 'sweph';
+import { calc, version } from 'sweph';
 
 export default function handler(req: VercelRequest, res: VercelResponse) {
   const { name = 'World' } = req.query
   return res.json({
     message: `Hello ${name}! ${version()}`,
+    test: calc(2435645, 0, 2)
   })
 }
