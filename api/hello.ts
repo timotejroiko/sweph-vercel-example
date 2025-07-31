a74d58ab381fa638bf8eff0440373346e42e7777
Timotej Rojko <33236065+timotejroiko@users.noreply.github.com>
2025-07-31 13:42:16 -0300

    test

--- api/hello.ts
+++ api/hello.ts
@@ -2,13 +2,13 @@ import type { VercelRequest, VercelResponse } from '@vercel/node'
 import { readdirSync } from 'node:fs';
 import { calc, set_ephe_path, version } from 'sweph';
 
-set_ephe_path("./ephe");
+set_ephe_path("./api/ephe");
 
 export default function handler(req: VercelRequest, res: VercelResponse) {
   const { name = 'World' } = req.query
   return res.json({
     message: `Hello ${name}! ${version()}`,
     test: calc(2435645, 0, 2),
-    files: readdirSync("./")
+    files: readdirSync("./api")
   })
 }
