21e9081b78412346df2fe60de7cf33da8efdf46f
Timotej Rojko <33236065+timotejroiko@users.noreply.github.com>
2025-07-31 13:35:53 -0300

    test

--- api/hello.ts
+++ api/hello.ts
@@ -1,5 +1,7 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node'
-import { calc, version } from 'sweph';
+import { calc, set_ephe_path, version } from 'sweph';
+
+set_ephe_path("../ephe");
 
 export default function handler(req: VercelRequest, res: VercelResponse) {
   const { name = 'World' } = req.query
